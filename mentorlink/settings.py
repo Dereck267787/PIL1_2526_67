7750ca1840520e3f58be97504753608f2d83d435
Dereck267787 <dereckfatoumbiofficiel@gmail.com>
2026-06-05 01:03:33 +0100

    Ajout des modules profiles, matching et messaging

--- mentorlink/settings.py
+++ mentorlink/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Nos modules IFRI_MentorLink
+    'profiles',
+    'matching',
+    'messaging',
 ]
 
 MIDDLEWARE = [
