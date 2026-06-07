35e71e591876e2ada5a29095f31cb8185406e541
zankpofrejus7-lab <zankpofrejus7@gmail.com>
2026-06-07 16:13:31 +0100

    Ajout des tables dans models.py et configuration de admin.py

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profil, Annonce, Message, Matching
 
 # Register your models here.
+admin.site.register(Profil)
+admin.site.register(Annonce)
+admin.site.register(Message)
+admin.site.register(Matching)
